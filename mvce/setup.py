690f5320d746b12fddd239b85d82c0add5427766
adambr02 <bryant.adams@nielsen.com>
2017-03-17 10:44:33 -0400

    Create working build, without cx_Oracle dependency

---
+++ mvce/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mvce',
+    version='0.0.1',
+    description='Minimum Viable Code Example',
+    author='Bryant Adams',
+    author_email='bryant.adams@claritas.com',
+    packages=find_packages()
+)
